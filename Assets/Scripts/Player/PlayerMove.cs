0096dcb2a42c1f0fd51df5340d26c0d66874d464
JavisKoo <115161833+JavisKoo@users.noreply.github.com>
2024-11-25 17:44:06 +0900

    씬 합체

--- Assets/Scripts/Player/PlayerMove.cs
+++ Assets/Scripts/Player/PlayerMove.cs
@@ -26,6 +26,7 @@ public class PlayerMove : BaseCharacter
     {
         animator = GetComponent<Animator>();
         spriteRenderer = GetComponent<SpriteRenderer>();
+        SetCharacterSettings(500);
     }
     // when button is pressed, move camera to the left smoothly
     private void Update()
