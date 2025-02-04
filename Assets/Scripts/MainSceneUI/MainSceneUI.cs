82530e326fa40218cee58a675b65b2f60ce39626
mingyu582 <104567047+mingyu582@users.noreply.github.com>
2025-02-04 22:03:03 +0900

    월드맵 area구현

--- Assets/Scripts/MainSceneUI/MainSceneUI.cs
+++ Assets/Scripts/MainSceneUI/MainSceneUI.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MainSceneUI : MonoBehaviour
 {
@@ -11,6 +12,12 @@ public class MainSceneUI : MonoBehaviour
 
     //dungeon data
     public Dungeon[] dungeonDatas;
+    //area
+    public Transform areaBase;
+    public GameObject[] areaTopUIPrefabs;
+    public Image[] areaBossImage;
+    public GameObject[] areaClearStar;
+    public Text[] areaDungeonNum;
 
     public void OpenUpgradePanel()
     {
@@ -25,27 +32,59 @@ public class MainSceneUI : MonoBehaviour
     //
     public void OpenAreaPanel(int type)
     {
+        //top
+        bool isClearTable = false;
+        GameObject go = Instantiate(areaTopUIPrefabs[type]);
+        go.transform.SetParent(areaBase,false);
+        areaTop areaTopScript = go.GetComponent<areaTop>();
+        areaTopScript.isDisable = true;
+        //items
+        for (int i = 0; i < areaDungeonNum.Length; i++) //스크롤 하나씩 돌음
+        {
+            
+            areaDungeonNum[i].text = (type + 1) + "-" + (i+1);
+            switch (type)
+            {
+                case 0:
+                    areaBossImage[i].sprite = dungeonDatas[i].dungeonBoss.itemIcon;
+                    isClearTable = dungeonDatas[i].isClear ? true : false;
+                    break;
+
+                case 1: //i가 3,4,5여야함
+                    areaBossImage[i].sprite = dungeonDatas[i+3].dungeonBoss.itemIcon;
+                    isClearTable = dungeonDatas[i+3].isClear ? true : false;
+                    break;
+
+                case 2://i가 6,7,8여야함
+                    areaBossImage[i].sprite = dungeonDatas[i+6].dungeonBoss.itemIcon;
+                    isClearTable = dungeonDatas[i+6].isClear ? true : false;
+                    break;
+
+                case 3://i가 9,10,11여야함
+                    areaBossImage[i].sprite = dungeonDatas[i+9].dungeonBoss.itemIcon;
+                    isClearTable = dungeonDatas[i+9].isClear ? true : false;
+                    break;
+            }
+
+            areaClearStar[i].SetActive(isClearTable);
+        }
+
         areaPanel.SetActive(true);
-    }
-
-    //x-n 스크롤 정보 넣기
-    public void SetScrollUI(ItemData bossData, bool isClear, int stageNum) //parameter 보스, 클리어, 스테이지 번호 필요
-    {
-
     }
 
     public void CloseAreaPanel()
     {
-        areaPanel?.SetActive(false);
+        areaPanel.SetActive(false);
     }
 
     public void OpenDungeonPanel()
     {
-        dungeonPanel?.SetActive(true);
+        CloseAreaPanel();
+        dungeonPanel.SetActive(true);
     }
 
     public void CloseDungeonPanel()
     {
-        dungeonPanel?.SetActive(false);
+        dungeonPanel.SetActive(false);
     }
 }
