f9181ac54819e65e0e54daf0c5dd3a6642da192d
BBAMO <115161833+JavisKoo@users.noreply.github.com>
2025-02-10 18:50:29 +0900

    mainscene

--- Assets/Scripts/Chracter/BaseCharacter.cs
+++ Assets/Scripts/Chracter/BaseCharacter.cs
@@ -129,6 +129,12 @@ namespace Chracter
         //get Data from ItemData
         public ItemData itemData;
         protected Coroutine Attackcoroutine;
+
+
+        [SerializeField]
+        protected AudioSource AttackSound;
+
+
         void Start()
         {
           
@@ -333,9 +339,11 @@ namespace Chracter
             if (currentEnemy)
             {
                 currentEnemy.collider.GetComponent<BaseCharacter>().TakeDamage(AttackDammage, Accuracy, Pierce, Attribute);
+                AttackSound.Play();
             }
 
             if (currentEnemys == null) return;
+            AttackSound.Play();
             foreach (var t in currentEnemys)
             {
                 t.collider.GetComponent<BaseCharacter>().TakeDamage(AttackDammage, Accuracy, Pierce, Attribute);
@@ -363,6 +371,7 @@ namespace Chracter
         {
             if (currentEnemy)
             {
+                AttackSound.Play();
                 GameObject rangedAttack = Instantiate(rangedAttackPrefab, rangedAttackSpawnPoint.position, Quaternion.identity);
                 rangedAttack.GetComponent<RangedAttack>().EnemySetting(currentEnemy, Enemy, AttackDammage, AttackRange, Accuracy,itemData.attackCountLimitValue);
             }
