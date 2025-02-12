ab7ced62c4fd3e7f308fa5ee507717a2a83393bb
mingyu582 <104567047+mingyu582@users.noreply.github.com>
2025-02-12 14:46:35 +0900

    골드수정

--- Assets/Scripts/Tower/Tower.cs
+++ Assets/Scripts/Tower/Tower.cs
@@ -129,6 +129,7 @@ public class Tower : BaseCharacter
     int towerLevel = 1;
     int[] upgradeCost = { 0, 25, 50, 100, 200, 400, 800, 1600, 3200 };
     int[] upgradeMaxGold = {0, 100, 330, 720, 1300, 2100, 3150, 4480, 6120, 8100 };
+    int[] upgradeMoneyUp = { 10, 15, 20, 30, 40, 55, 70, 90, 120 };
     public Text towerLevelText;
     public Text upgradeCostText;
     public UnityEngine.UI.Button upgradeBtn;
@@ -150,7 +151,7 @@ public class Tower : BaseCharacter
             CurrentHealth += 200;
             MaxHealth += 200;
             towerHPSlider.maxValue = MaxHealth;
-            goldPerSec += 5;
+            goldPerSec = upgradeMoneyUp[towerLevel];
             Armor++;
 
             //ui
